2c94d7fc96ccb31577823009a9b39c09eb871fa6
Stefani Radevska <stefaniradevska@i3d.net>
2024-01-20 20:38:13 +0100

    adding edits

--- code/index.php
+++ code/index.php
@@ -1,5 +1,16 @@
 <?php
 
-echo "Hello World";
+$servername = "mysql";
+$username = "root";
+$password = "lemp";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 
 ?>
